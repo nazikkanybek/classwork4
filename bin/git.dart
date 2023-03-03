4855547007603bd5747f3eca7a75e58078a6c79f
unknown <nazikmanas9@gmail.com>
2023-03-03 12:56:15 +0300

    first commit

---
+++ bin/git.dart
@@ -0,0 +1,7 @@
+import 'package:git/git.dart' as git;
+
+void main(List<String> arguments) {
+  List a = [1, 2, 3];
+  a.contains(3);
+  print(a.contains(3));
+}
---
+++ test/git_test.dart
@@ -0,0 +1,8 @@
+import 'package:git/git.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
